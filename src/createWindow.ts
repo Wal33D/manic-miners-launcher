43efa68f34e3bce73c6a78ef5bef496892b47693
Waleed Gudah <aquataze@yahoo.com>
2024-05-08 05:17:09 -0500

    Updated createWindow logic and styles in index.css

--- src/createWindow.ts
+++ src/createWindow.ts
@@ -8,14 +8,15 @@ export const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 700,
+    frame: false, // This will remove the frame
+    transparent: true,
+    autoHideMenuBar: true, // This will hide the menu bar
+    roundedCorners: true,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
       nodeIntegration: true,
       contextIsolation: false,
     },
-    frame: false, // This will remove the frame
-    transparent: true,
-    autoHideMenuBar: true, // This will hide the menu bar
   });
 
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
