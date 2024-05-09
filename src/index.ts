2754e689bc0f418564f0a06331352edeeaefd5ee
Waleed Gudah <aquataze@yahoo.com>
2024-05-09 06:46:51 -0500

    Refactor index.ts and versionSelect.ts for improved functionality

--- src/index.ts
+++ src/index.ts
@@ -30,18 +30,18 @@ ipcMain.on('request-version-information', async (event, arg) => {
   switch (arg) {
     case 'fetchVersions':
       try {
-        const existingInstalls = await checkInstalledVersionsWithExe();
+        const { existingInstalls } = await checkInstalledVersionsWithExe();
 
         // Fetch version data, default to 'all' if not specified in the arg
         const versionData = await fetchVersions({ versionType: 'all' });
         event.reply('version-information-reply', { versions: versionData.versions, currentlyInstalledVersions: existingInstalls });
       } catch (error) {
         console.error('Error fetching versions:', error);
-        event.reply('action-reply', { error: error.message }); // Send error message back to renderer for handling
+        event.reply('version-information-reply', { error: error.message }); // Send error message back to renderer for handling
       }
       break;
     default:
-      event.reply('action-reply', { error: 'Received an unknown command' });
+      event.reply('version-information-reply', { error: 'Received an unknown command' });
   }
 });
 
--- src/renderer/versionSelect.ts
+++ src/renderer/versionSelect.ts
@@ -4,20 +4,28 @@ export const loadVersionSelect = (): void => {
   //@ts-ignore
   window.electronAPI?.receive('version-information-reply', data => {
     console.log(data);
-
+    const { versions, currentlyInstalledVersions } = data;
     const versionSelect = document.getElementById('versionSelect');
     if (versionSelect) {
       while (versionSelect.children.length > 1) {
         versionSelect.removeChild(versionSelect.lastChild);
       }
 
-      if (Array.isArray(data) && data.length > 0) {
-        data.forEach(version => {
+      if (Array.isArray(versions) && versions.length > 0) {
+        versions.forEach(version => {
           const option = document.createElement('option');
           option.value = version.identifier;
           option.textContent = version.displayName;
           versionSelect.appendChild(option);
         });
+
+        if (Array.isArray(currentlyInstalledVersions) && currentlyInstalledVersions.length > 0) {
+          const firstInstalledVersion = currentlyInstalledVersions[0];
+          //@ts-ignore
+          versionSelect.value = firstInstalledVersion.identifier;
+        } else {
+          console.error('No currently installed versions data received or data is not an array.');
+        }
       } else {
         console.error('No versions data received or data is not an array.');
       }
