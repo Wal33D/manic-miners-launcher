f0fcbf7edbe585c30eba4814bfb7a5dfd5629ce1
Waleed Gudah <aquataze@yahoo.com>
2024-05-13 01:35:42 -0500

    Updated index page styling and renderer logic, modified installation panel HTML

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -3,9 +3,9 @@ import './index.css';
 import '../ui/styles/mainMenuModal.css';
 import { setupTopNav } from './components/setupTopNav';
 import { setupPlayButton } from './components/setupPlayButton';
-import { topNavbarElement } from '../ui/partials/topNavbarElement';
-import { installPanelHtml } from '../ui/partials/installPanelHtml';
-import { progressBarElement } from '../ui/partials/progressBarElement';
+//import { topNavbarElement } from '../ui/partials/topNavbarElement';
+//import { installPanelHtml } from '../ui/partials/installPanelHtml';
+//import { progressBarElement } from '../ui/partials/progressBarElement';
 import { setupInstallButton } from './components/setupInstallButton';
 import { setupDirectoryDialog } from './components/setupDirectoryDialog';
 import { initializeVersionSelect } from './components/initializeVersionSelect';
@@ -17,13 +17,12 @@ initializeVersionSelect();
 document.addEventListener('DOMContentLoaded', () => {
   const topNav = document.getElementById('top-navbar-container');
   if (topNav) {
-    topNav.innerHTML = topNavbarElement;
     setupTopNav(topNav);
   }
 
   const footer = document.getElementById('bottom-navbar-container');
   if (footer) {
-    footer.innerHTML = progressBarElement;
+    //   footer.innerHTML = progressBarElement;
   }
 
   const installerMenuModal = document.getElementById('installer-menu-modal-container');
@@ -33,8 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const installPaneContainer = document.getElementById('install-pane-container');
   if (installPaneContainer) {
-    installPaneContainer.innerHTML = installPanelHtml;
-
     const playButton = document.getElementById('playButton') as HTMLButtonElement;
     const versionSelect: any = document.getElementById('versionSelect') as HTMLSelectElement;
     const installButton = document.getElementById('installButton') as HTMLButtonElement;
