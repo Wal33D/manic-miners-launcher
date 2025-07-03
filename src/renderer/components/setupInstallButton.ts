4903cfd88b4d8ea6d2d89f44fbec4a1d134db0b6
Waleed Judah <aquataze@yahoo.com>
2025-07-03 00:18:58 -0400

    Define electron API types and clean ts ignores

--- src/renderer/components/setupInstallButton.ts
+++ src/renderer/components/setupInstallButton.ts
@@ -19,25 +19,20 @@ export function setupInstallButton(installButton: HTMLButtonElement, installPath
     setDisabledAppearance(installPathInput, true);
 
     // Send the download request to the main process
-    //@ts-ignore
     window.electronAPI.send(IPC_CHANNELS.DOWNLOAD_VERSION, {
       version: versionIdentifier,
       downloadPath: downloadPath,
     });
   });
 
-  //@ts-ignore
   window.electronAPI.receive(IPC_CHANNELS.DOWNLOAD_PROGRESS, ({ progress, status }) => {
     updateStatus(progress, status);
   });
 
-  //@ts-ignore
   window.electronAPI.receive(IPC_CHANNELS.DOWNLOAD_VERSION, result => {
     console.log(result.message);
     if (result.downloaded) {
-      //@ts-ignore
       window.electronAPI.send(IPC_CHANNELS.PLAY_SOUND); // Request the main process to play the success sound
-      //@ts-ignore
       window.electronAPI.send(IPC_CHANNELS.ALL_VERSION_INFO); // Request updated version list
     } else {
       alert('Failed to download the version: ' + result.message);
@@ -49,10 +44,8 @@ export function setupInstallButton(installButton: HTMLButtonElement, installPath
     setDisabledAppearance(installPathInput, false);
   });
 
-  //@ts-ignore
   window.electronAPI.receive(IPC_CHANNELS.VERSIONS_UPDATED, () => {
     // Re-fetch version info to update the UI
-    //@ts-ignore
     window.electronAPI.send(IPC_CHANNELS.ALL_VERSION_INFO);
   });
 }
