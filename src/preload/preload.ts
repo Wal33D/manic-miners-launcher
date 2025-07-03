a74f1a2e77380fe495f8c217b5a0040391e91f12
Waleed Judah <aquataze@yahoo.com>
2025-07-02 23:30:52 -0400

    Secure IPC by validating channels

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,11 +1,40 @@
 const { shell, contextBridge, ipcRenderer } = require('electron');
+const { IPC_CHANNELS } = require('../main/ipcHandlers/ipcChannels');
+
+const validSendChannels = [
+  IPC_CHANNELS.GET_DIRECTORIES,
+  IPC_CHANNELS.LAUNCH_GAME,
+  IPC_CHANNELS.DOWNLOAD_VERSION,
+  IPC_CHANNELS.PLAY_SOUND,
+  IPC_CHANNELS.ALL_VERSION_INFO,
+  IPC_CHANNELS.SET_SELECTED_VERSION,
+  IPC_CHANNELS.GET_URLS,
+  IPC_CHANNELS.GET_LEVELS,
+  'open-directory-dialog',
+];
+
+const validReceiveChannels = [
+  IPC_CHANNELS.GET_DIRECTORIES,
+  IPC_CHANNELS.LAUNCH_GAME,
+  IPC_CHANNELS.DOWNLOAD_PROGRESS,
+  IPC_CHANNELS.DOWNLOAD_VERSION,
+  IPC_CHANNELS.VERSIONS_UPDATED,
+  IPC_CHANNELS.ALL_VERSION_INFO,
+  IPC_CHANNELS.GET_URLS,
+  IPC_CHANNELS.GET_LEVELS,
+  'directory-selected',
+];
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  send: (channel: string, data: any) => {
-    ipcRenderer.send(channel, data);
+  send: (channel: string, data?: any) => {
+    if (validSendChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
   },
   receive: (channel: string, func: Function) => {
-    ipcRenderer.on(channel, (event, ...args) => func(...args));
+    if (validReceiveChannels.includes(channel)) {
+      ipcRenderer.on(channel, (_event, ...args) => func(...args));
+    }
   },
   openExternal: shell.openExternal,
 });
