bdfc70653c88f2e0f9bc528baa843b3ef9c515a8
Waleed Judah <aquataze@yahoo.com>
2025-07-08 04:26:55 -0400

    refactor tests to use archiver/jszip

--- test/unpackVersion.test.ts
+++ test/unpackVersion.test.ts
@@ -2,23 +2,14 @@ import test from 'node:test';
 import assert from 'node:assert';
 import fs from 'fs';
 import path from 'path';
-import { execSync } from 'child_process';
+import { createTraversalZip } from './helpers/archive';
 import { unpackVersion } from '../src/functions/unpackVersion';
 import * as fetchVersionsModule from '../src/api/fetchVersions';
 
-function createMaliciousZip(dir: string) {
-  const zipDir = path.join(dir, 'zip');
-  fs.mkdirSync(zipDir);
-  const outside = path.join(dir, 'evil.txt');
-  fs.writeFileSync(outside, 'evil');
-  execSync('zip mal.zip ../evil.txt', { cwd: zipDir });
-  fs.renameSync(path.join(zipDir, 'mal.zip'), path.join(dir, 'mal.zip'));
-  fs.rmSync(zipDir, { recursive: true, force: true });
-}
 
 test('unpackVersion rejects archives with path traversal entries', async () => {
   const dir = fs.mkdtempSync(path.join(process.cwd(), 'tmp-'));
-  createMaliciousZip(dir);
+  await createTraversalZip(dir, 'mal.zip');
 
   const original = fetchVersionsModule.fetchVersions;
   (fetchVersionsModule as any).fetchVersions = async () => ({
