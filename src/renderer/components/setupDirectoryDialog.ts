1f32befe7fb0c5858dd0911d3cc689a90997a65d
Waleed Judah <aquataze@yahoo.com>
2025-07-08 04:24:14 -0400

    feat: add directory dialog ipc channels

--- src/renderer/components/setupDirectoryDialog.ts
+++ src/renderer/components/setupDirectoryDialog.ts
@@ -1,12 +1,14 @@
+import { IPC_CHANNELS } from '../../main/ipcHandlers/ipcChannels';
+
 export function setupDirectoryDialog(installPathInput: HTMLInputElement) {
   installPathInput.addEventListener('click', () => {
     // Trigger the directory dialog on click
-    window.electronAPI.send('open-directory-dialog');
+    window.electronAPI.send(IPC_CHANNELS.OPEN_DIRECTORY_DIALOG);
   });
 
   // Handler to receive the selected directory path and update the input field
-  window.electronAPI.removeAllListeners('directory-selected');
-  window.electronAPI.receive('directory-selected', (path: any) => {
+  window.electronAPI.removeAllListeners(IPC_CHANNELS.DIRECTORY_SELECTED);
+  window.electronAPI.receive(IPC_CHANNELS.DIRECTORY_SELECTED, (path: any) => {
     installPathInput.value = path as string;
   });
 }
