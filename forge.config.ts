e56ad0fd9afbc74f4b0d1d9344ffb256f3ab0a5c
Waleed Judah <aquataze@yahoo.com>
2025-07-09 01:19:56 -0400

    Allow unsafe-eval during development

--- forge.config.ts
+++ forge.config.ts
@@ -39,7 +39,7 @@ const config: ForgeConfig = {
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
         "font-src 'self' https://fonts.gstatic.com; " +
         "img-src 'self' data:; " +
-        "script-src 'self' 'unsafe-inline'",
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       contentSecurityPolicy:
         "default-src 'self'; " +
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
