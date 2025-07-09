4fa2f0dda64bc9949288b22b9243488f74b4ab02
Waleed Judah <aquataze@yahoo.com>
2025-07-09 03:04:22 -0400

    refactor: switch ui to tailwind and daisyui

--- src/ui/partials/messageBoxElement.ts
+++ src/ui/partials/messageBoxElement.ts
@@ -1,6 +1,6 @@
 export const messageBoxElement = `
-<div id="message-pane-container" class="bg-dark text-light rounded draggableArea"></div>
+<div id="message-pane-container" class="bg-neutral text-neutral-content rounded draggableArea"></div>
 <div class="p-3">
-    <label for="messageBox" class="form-label">Mission Updates</label>
+    <label for="messageBox" class="label">Mission Updates</label>
 </div>
 `;
--- src/ui/partials/topNavbarElement.ts
+++ src/ui/partials/topNavbarElement.ts
@@ -1,13 +1,15 @@
 export const topNavbarElement = `
-<nav class="navbar navbar-expand-lg navbar-dark py-3 fixed-top">
-    <div class="container-fluid">
-        <a id="navbar-main-menu-modal-btn" class="navbar-brand d-flex align-items-center not-draggable" href="#">
-            <div id="brand-image" class="d-inline-block align-text-top"></div>
+<nav class="navbar bg-neutral text-neutral-content p-3 fixed-top">
+    <div class="flex-1">
+        <a id="navbar-main-menu-modal-btn" class="btn btn-ghost normal-case text-xl not-draggable flex items-center" href="#">
+            <div id="brand-image" class="inline-block align-top mr-2"></div>
             Manic Miners HQ
         </a>
-        <a id="navbar-menu-btn" class="icon-button not-draggable ms-auto" href="#">
+    </div>
+    <div class="flex-none">
+        <button id="navbar-menu-btn" class="btn btn-ghost not-draggable">
             <i class="fas fa-bars"></i>
-        </a>
+        </button>
     </div>
 </nav>
 `;
