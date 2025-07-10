d260c00978c6882ffab8d7b4d75c2c89c8426869
Waleed Judah <aquataze@yahoo.com>
2025-07-10 01:25:24 -0400

    fix lint script and start cleaning code

--- launcher-gui/src/App.tsx
+++ launcher-gui/src/App.tsx
@@ -1,15 +1,15 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import { LauncherHeader } from "@/components/LauncherHeader";
-import { InstalledLevelsProvider } from "./hooks/useInstalledLevels";
-import Index from "./pages/Index";
-import LevelDownloader from "./pages/LevelDownloader";
-import Library from "./pages/Library";
-import MapGenerator from "./pages/MapGenerator";
-import NotFound from "./pages/NotFound";
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { LauncherHeader } from '@/components/LauncherHeader';
+import { InstalledLevelsProvider } from './hooks/useInstalledLevels';
+import Index from './pages/Index';
+import LevelDownloader from './pages/LevelDownloader';
+import Library from './pages/Library';
+import MapGenerator from './pages/MapGenerator';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
 
