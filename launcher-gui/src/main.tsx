0a1b70bcbcf2989985816c02d8557d58258c7156
Waleed Judah <aquataze@yahoo.com>
2025-07-10 01:09:11 -0400

    .

--- launcher-gui/src/main.tsx
+++ launcher-gui/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 // Always land on the same route as the Home button ("/").
 // This avoids hitting the NotFound page on first launch.
 if (window.location.hash !== '#/') {
-  window.location.hash = '#/';
+  window.location.hash = '/';
 }
 
 createRoot(document.getElementById('root')!).render(<App />);
