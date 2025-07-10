e0964364786e2abdde35c4b0a26ade7a5afc0c8e
Waleed Judah <aquataze@yahoo.com>
2025-07-09 23:59:42 -0400

    fix: load root path without hash

--- launcher-gui/src/main.tsx
+++ launcher-gui/src/main.tsx
@@ -2,11 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Always land on the same route as the Home button ("/").
-// This avoids hitting the NotFound page on first launch.
-if (window.location.hash !== '#/') {
-
-  window.location.hash = '#/';
-}
-
 createRoot(document.getElementById('root')!).render(<App />);
+
