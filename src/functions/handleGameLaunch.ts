c2dc0e311f5a91d2982127aa7511bea73b8c0843
Waleed Judah <aquataze@yahoo.com>
2025-07-10 03:57:42 -0400

    Add Wine support for non-Windows

--- src/functions/handleGameLaunch.ts
+++ src/functions/handleGameLaunch.ts
@@ -1,20 +1,32 @@
 import { logToFile } from '../logger';
 import { launchExecutable } from './launchExecutable';
 import { fetchInstalledVersions } from './fetchInstalledVersions';
+import { checkCompatLauncher } from './checkCompatLauncher';
 
 /**
  * Function to handle the launching of a specific game version or the first available version if no identifier is provided.
  * @param versionIdentifier Optional identifier for a specific version to launch.
- * @returns Promise<boolean> indicating whether the game was successfully launched.
+ * @returns Result object with status and message.
  */
-export const handleGameLaunch = async ({ versionIdentifier }: { versionIdentifier?: string }): Promise<boolean> => {
+export const handleGameLaunch = async ({
+  versionIdentifier,
+}: {
+  versionIdentifier?: string;
+}): Promise<{ status: boolean; message: string }> => {
   try {
     logToFile({ message: `Received request to launch game version: '${versionIdentifier || 'No specific version requested'}'` });
+
+    if (!checkCompatLauncher()) {
+      const message = 'Wine is required to launch the game on non-Windows systems.';
+      return { status: false, message };
+    }
+
     const installedVersions = await fetchInstalledVersions();
 
     if (!installedVersions.status || !installedVersions.installedVersions || installedVersions.installedVersions.length === 0) {
-      logToFile({ message: 'No installations found or failed to fetch installations.' });
-      return false;
+      const message = 'No installations found or failed to fetch installations.';
+      logToFile({ message });
+      return { status: false, message };
     }
 
     // Determine which version to launch
@@ -27,22 +39,27 @@ export const handleGameLaunch = async ({ versionIdentifier }: { versionIdentifie
 
     // Check if the version has an executable to launch
     if (!versionToLaunch.executablePath) {
-      logToFile({ message: `No executable files found for the selected version: ${versionToLaunch.identifier}` });
-      return false;
+      const message = `No executable files found for the selected version: ${versionToLaunch.identifier}`;
+      logToFile({ message });
+      return { status: false, message };
     }
 
     const launchResults = await launchExecutable({ executablePath: versionToLaunch.executablePath });
     logToFile({ message: launchResults.message });
 
     if (launchResults.status) {
-      logToFile({ message: `The executable for version '${versionToLaunch.identifier}' ran successfully.` });
-      return true;
+      const message = `The executable for version '${versionToLaunch.identifier}' ran successfully.`;
+      logToFile({ message });
+      return { status: true, message };
     } else {
-      logToFile({ message: `The executable for version '${versionToLaunch.identifier}' failed with exit code: ${launchResults.exitCode}` });
-      return false;
+      const message = `The executable for version '${versionToLaunch.identifier}' failed with exit code: ${launchResults.exitCode}`;
+      logToFile({ message });
+      return { status: false, message };
     }
   } catch (error) {
-    logToFile({ message: `Failed to launch game: ${error.message}` });
-    return false;
+    const err = error as Error;
+    const message = `Failed to launch game: ${err.message}`;
+    logToFile({ message });
+    return { status: false, message };
   }
 };
