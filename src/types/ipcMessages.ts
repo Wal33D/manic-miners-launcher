53bbfe76a571d397e0c151d9329b12457c84ad24
Waleed Judah <aquataze@yahoo.com>
2025-07-12 05:15:09 -0400

    feat: show real download metrics

--- src/types/ipcMessages.ts
+++ src/types/ipcMessages.ts
@@ -3,6 +3,14 @@ import { Version } from '../api/versionTypes';
 export interface ProgressStatus {
   progress?: number;
   status?: string;
+  /** name of the file currently being downloaded */
+  fileName?: string;
+  /** total size of the file in bytes */
+  totalSize?: number;
+  /** current download speed in bytes per second */
+  speedBytesPerSec?: number;
+  /** estimated time remaining in seconds */
+  etaSeconds?: number;
 }
 
 export interface DownloadResult {
