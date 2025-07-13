793edadb458c59b1d7162e5ceed179d7e30dcee6
Waleed Judah <aquataze@yahoo.com>
2025-07-13 01:34:19 -0400

    fix: restore repair action for archived versions

--- launcher-gui/src/pages/GameVersions.tsx
+++ launcher-gui/src/pages/GameVersions.tsx
@@ -15,7 +15,7 @@ const GameVersions = () => {
 
   return (
     <div className="h-full flex flex-col overflow-y-auto relative">
-      <GameNotifications 
+      <GameNotifications
         notifications={notifications}
         onDismiss={handleDismissNotification}
       />
