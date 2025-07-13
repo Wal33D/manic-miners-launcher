fad56483ee5279a21950f9f6630bda2a946d62ca
Waleed Judah <aquataze@yahoo.com>
2025-07-13 04:39:16 -0400

    Restore latest version section

--- manic-miners-sync-server/src/archived/ensureDirectoryExists.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export const ensureDirectoryExists = async (dir: fs.PathLike) => {
-   try {
-      await fs.promises.stat(dir);
-   } catch {
-      await fs.promises.mkdir(dir, { recursive: true });
-   }
-};
